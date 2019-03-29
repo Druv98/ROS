d6ba3f3f7cde45d0c87728d81c2a878f07403109
Druvang Wagh <druvangjitendra.wagh2016@vitstudent.ac.in>
2019-03-30 00:08:21 +0530

    Add files via upload

---
+++ beginner_tutorials/src/IoT_publisher.cpp
@@ -0,0 +1,90 @@
+#include "ros/ros.h"
+#include "beginner_tutorials/IoTSensor.h"
+
+
+// *
+//  * This tutorial demonstrates simple sending of messages over the ROS system.
+int main(int argc, char **argv)
+{
+  /**
+   * The ros::init() function needs to see argc and argv so that it can perform
+   * any ROS arguments and name remapping that were provided at the command line.
+   * For programmatic remappings you can use a different version of init() which takes
+   * remappings directly, but for most command-line programs, passing argc and argv is
+   * the easiest way to do it.  The third argument to init() is the name of the node.
+   *
+   * You must call one of the versions of ros::init() before using any other
+   * part of the ROS system.
+   */
+  ros::init(argc, argv, "IoT_publisher");
+
+  /**
+   * NodeHandle is the main access point to communications with the ROS system.
+   * The first NodeHandle constructed will fully initialize this node, and the last
+   * NodeHandle destructed will close down the node.
+   */
+  ros::NodeHandle n;
+
+  /**
+   * The advertise() function is how you tell ROS that you want to
+   * publish on a given topic name. This invokes a call to the ROS
+   * master node, which keeps a registry of who is publishing and who
+   * is subscribing. After this advertise() call is made, the master
+   * node will notify anyone who is trying to subscribe to this topic name,
+   * and they will in turn negotiate a peer-to-peer connection with this
+   * node.  advertise() returns a Publisher object which allows you to
+   * publish messages on that topic through a call to publish().  Once
+   * all copies of the returned Publisher object are destroyed, the topic
+   * will be automatically unadvertised.
+   *
+   * The second parameter to advertise() is the size of the message queue
+   * used for publishing messages.  If messages are published more quickly
+   * than we can send them, the number here specifies how many messages to
+   * buffer up before throwing some away.
+   */
+  ros::Publisher IoTSensor_pub = n.advertise<beginner_tutorials::IoTSensor>("IoTSensor_publisher_topic", 1000);
+  // ros::Publisher IoTSensor_pub = n.advertise<IoTSensor::float>("IoTSensor publisher node", 1000);
+  // ros::Publisher IoTSensor_pub = n.advertise<IoTSensor::string>("IoTSensor publisher node", 1000);
+  // ros::Publisher IoTSensor_pub = n.advertise<IoTSensor::float>("IoTSensor publisher node", 1000);
+
+  ros::Rate loop_rate(10);
+
+  /**
+   * A count of how many messages we have sent. This is used to create
+   * a unique string for each message.
+   */
+  int count = 0;
+  while (ros::ok())
+  {
+    /**
+     * This is a message object. You stuff it with data, and then publish it.
+     */
+    beginner_tutorials::IoTSensor msg;
+    
+    msg.name="Druvang";
+    msg.temperature=42;
+    msg.humidity=10;
+    msg.id=count;
+    
+    // ROS_INFO("%d", msg.id);
+    // ROS_INFO("%s[]", msg.name);
+    // ROS_INFO("%.2f", msg.temperature);
+    // ROS_INFO("%.2f", msg.humidity);
+    std::cout<<"Message id:"<<(int)msg.id<<"Name of the message"<<msg.name<<"Temperature and Humidity: "<<msg.temperature<<msg.humidity<<std::endl;
+    /**
+     * The publish() function is how you send messages. The parameter
+     * is the message object. The type of this object must agree with the type
+     * given as a template parameter to the advertise<>() call, as was done
+     * in the constructor above.
+     */
+    IoTSensor_pub.publish(msg);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+    ++count;
+  }
+
+
+  return 0;
+}
